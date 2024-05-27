397cfc04bf0b0bd4fedc8ce959612559fa68933c
Antony Martinez Ruiz <antonymaruiz02@gmail.com>
2024-05-27 10:30:33 -0500

    Create project with spring initializr

---
+++ src/main/java/com/riwi/learningRiwi/LearningRiwiApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.learningRiwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRiwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningRiwiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/learningRiwi/LearningRiwiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.learningRiwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRiwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
