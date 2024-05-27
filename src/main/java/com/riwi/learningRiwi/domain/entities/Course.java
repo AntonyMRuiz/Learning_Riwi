f7f3f2daa0042743a123939d8b3d15d96eb4465b
Antony Martinez Ruiz <antonymaruiz02@gmail.com>
2024-05-27 12:37:44 -0500

    Create course entity

--- src/main/java/com/riwi/learningRiwi/domain/entities/Course.java
+++ src/main/java/com/riwi/learningRiwi/domain/entities/Course.java
@@ -1,33 +1,34 @@
 package com.riwi.learningRiwi.domain.entities;
 
-import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "enrollment")
+@Entity(name = "course")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Enrollment {
+public class Course {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private LocalDate date;
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Lob
+    private String description;
 
     /* 
-    private User studentId;
-    private Course courseId;
+    private User instructorId;
     */
 }
